edabcd1ad8e99e68a0024e3f5a9247019eacc69b
kamaltaghiyev <83601296+kamaltaghiyev@users.noreply.github.com>
2021-05-31 13:03:42 +0400

    dasdasasd

--- Cli_tool/src/main/java/Main.java
+++ Cli_tool/src/main/java/Main.java
@@ -2,9 +2,10 @@ import Service.Impl.GuiServiceImpl;
 import exceptions.RatingOutOfBoundException;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, RatingOutOfBoundException {
+    public static void main(String[] args) throws SQLException, RatingOutOfBoundException, ParseException {
         GuiServiceImpl guiService = new GuiServiceImpl();
         guiService.gui();
     }
